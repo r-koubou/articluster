c983fd587c1dc874235d44f737903abc9748ad50
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-04-22 00:38:13 +0900

    不要ファイル削除

--- articluster/Features/UniversalDefinition/Infrastructures/Infrastructures.Json/Model/AssignmentModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ArtiCluster.Features.UniversalDefinition.Infrastructures.Json.Model;
-
-public class AssignmentModel
-{
-    public MidiMessageModel MidiMessage { get; set; } = new();
-    public Dictionary<string, string> Extra { get; set; } = new();
-}
--- articluster/Features/UniversalDefinition/Infrastructures/Infrastructures.Json/Model/MidiMessageModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ArtiCluster.Features.UniversalDefinition.Infrastructures.Json.Model;
-
-public class MidiMessageModel
-{
-    public int Status { get; set; }
-    public int Data1 { get; set; }
-    public int Data2 { get; set; }
-}
