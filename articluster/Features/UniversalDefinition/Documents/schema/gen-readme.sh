2756f300a4f6bc1b18ecd2f31b48ec3440728c35
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-04-24 00:04:04 +0900

    typo

--- articluster/Features/UniversalDefinition/Documents/schema/gen-readme.sh
+++ articluster/Features/UniversalDefinition/Documents/schema/gen-readme.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-uvx jsonschema2md --show-examples all --examples-as-yaml  ./universal-definition-scema.json ./README.md
+uvx jsonschema2md --show-examples all --examples-as-yaml  ./universal-definition-schema.json ./README.md
