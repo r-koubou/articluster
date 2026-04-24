88eb3666e648808915b864b8e9faffc301c22a79
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-04-24 16:30:03 +0900

    16進数固定出力をやめた

--- articluster/Features/UniversalDefinition/Infrastructures/Infrastructures.Yaml/SerializationConstants.cs
+++ articluster/Features/UniversalDefinition/Infrastructures/Infrastructures.Yaml/SerializationConstants.cs
@@ -7,9 +7,9 @@ internal static class SerializationConstants
     public static readonly ISerializer DefaultSerializer
         = new SerializerBuilder()
          .ConfigureDefaultValuesHandling( DefaultValuesHandling.OmitEmptyCollections | DefaultValuesHandling.OmitDefaults )
-         .WithTypeConverter( new YamlHexIntConverter() ).Build();
+         .Build();
 
     public static readonly IDeserializer DefaultDeserializer
         = new DeserializerBuilder()
-         .WithTypeConverter( new YamlHexIntConverter() ).Build();
+         .Build();
 }
