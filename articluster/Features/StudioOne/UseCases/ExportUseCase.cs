d59f393503c1753a6e9403ab7a28af7e7839092a
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-04-25 22:39:21 +0900

    ユースケース作成

---
+++ articluster/Features/StudioOne/UseCases/ExportUseCase.cs
@@ -0,0 +1,33 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+using ArtiCluster.Commons;
+using ArtiCluster.Features.StudioOne.Gateways;
+using ArtiCluster.Shared.Domain.UniversalDefinitions;
+using ArtiCluster.Shared.IO.Abstractions;
+
+namespace ArtiCluster.Features.StudioOne.UseCases;
+
+public sealed class ExportInputPort
+{
+    public UniversalDefinitionProductSet Source { get; init; }
+    public IDefinitionExporter Exporter { get; init; }
+    public ITextContentWriter ContentWriter { get; init; }
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public ExportInputPort( UniversalDefinitionProductSet source, IDefinitionExporter exporter, ITextContentWriter contentWriter )
+    {
+        Exporter      = exporter;
+        ContentWriter = contentWriter;
+        Source        = source;
+    }
+}
+
+public sealed class ExportUseCase
+{
+    // ReSharper disable once MemberCanBeMadeStatic.Global
+    public async Task<Result<Unit, ExportReason>> ExecuteAsync( ExportInputPort input, CancellationToken cancellationToken = default )
+    {
+        return await input.Exporter.ExportAsync( input.ContentWriter, input.Source, cancellationToken );
+    }
+}
