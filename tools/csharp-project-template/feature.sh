7345937babb6fc2639590f3a52cd7cdca2197b97
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-04-25 23:13:15 +0900

    引数に . を含む場合はサブディレクトリを作成するようにした

--- tools/csharp-project-template/feature.sh
+++ tools/csharp-project-template/feature.sh
@@ -11,19 +11,24 @@ fi
 ./module.sh Features.$1.Infrastructures
 ./module.sh Features.$1.UseCases --no-test
 
+# Setup output directory (Create subdirectories if '.' is included in the feature name)
+feature_dir="out/Features/$1"
+feature_dir="${feature_dir//.//}"
+
+mkdir -p "$feature_dir"
+
 # rename directories : remove $1 from $1.xxx
-mkdir -p out/$1
-mv out/Features.$1.Domain out/$1/Domain
-mv out/Features.$1.Facades out/$1/Facades
-mv out/Features.$1.Gateways out/$1/Gateways
-mv out/Features.$1.Infrastructures out/$1/Infrastructures
-mv out/Features.$1.Infrastructures.Tests out/$1/Infrastructures.Tests
-mv out/Features.$1.UseCases out/$1/UseCases
+mv out/Features.$1.Domain $feature_dir/Domain
+mv out/Features.$1.Facades $feature_dir/Facades
+mv out/Features.$1.Gateways $feature_dir/Gateways
+mv out/Features.$1.Infrastructures $feature_dir/Infrastructures
+mv out/Features.$1.Infrastructures.Tests $feature_dir/Infrastructures.Tests
+mv out/Features.$1.UseCases $feature_dir/UseCases
 
 # rename *.scproj : remove Features. from Features.$1.xxx.csproj
-mv out/$1/Domain/Features.$1.Domain.csproj out/$1/Domain/$1.Domain.csproj
-mv out/$1/Facades/Features.$1.Facades.csproj out/$1/Facades/$1.Facades.csproj
-mv out/$1/Gateways/Features.$1.Gateways.csproj out/$1/Gateways/$1.Gateways.csproj
-mv out/$1/Infrastructures/Features.$1.Infrastructures.csproj out/$1/Infrastructures/$1.Infrastructures.csproj
-mv out/$1/Infrastructures.Tests/Features.$1.Infrastructures.Tests.csproj out/$1/Infrastructures.Tests/$1.Infrastructures.Tests.csproj
-mv out/$1/UseCases/Features.$1.UseCases.csproj out/$1/UseCases/$1.UseCases.csproj
+mv $feature_dir/Domain/Features.$1.Domain.csproj $feature_dir/Domain/$1.Domain.csproj
+mv $feature_dir/Facades/Features.$1.Facades.csproj $feature_dir/Facades/$1.Facades.csproj
+mv $feature_dir/Gateways/Features.$1.Gateways.csproj $feature_dir/Gateways/$1.Gateways.csproj
+mv $feature_dir/Infrastructures/Features.$1.Infrastructures.csproj $feature_dir/Infrastructures/$1.Infrastructures.csproj
+mv $feature_dir/Infrastructures.Tests/Features.$1.Infrastructures.Tests.csproj $feature_dir/Infrastructures.Tests/$1.Infrastructures.Tests.csproj
+mv $feature_dir/UseCases/Features.$1.UseCases.csproj $feature_dir/UseCases/$1.UseCases.csproj
