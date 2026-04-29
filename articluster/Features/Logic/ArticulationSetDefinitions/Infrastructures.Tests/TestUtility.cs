598c382d5e79f106e6e7ce860b5bb118dc8ffcb3
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-04-30 01:42:17 +0900

    plist形式の書き込み処理実装（plistライブラリ使用でORMなし）

---
+++ articluster/Features/Logic/ArticulationSetDefinitions/Infrastructures.Tests/TestUtility.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using ArtiCluster.Shared.Domain.MidiMessages.Model;
+using ArtiCluster.Shared.Domain.UniversalDefinitions.Model;
+
+using NUnit.Framework;
+
+namespace ArtiCluster.Features.Logic.ArticulationSetDefinitions.Infrastructures.Tests;
+
+internal static class TestUtility
+{
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static readonly string TestDataDirectoryRoot = Path.Combine( TestContext.CurrentContext.TestDirectory, "TestData" );
+
+    public static string MakeTestDataPath( string relativePath )
+        => Path.Combine( TestDataDirectoryRoot, relativePath );
+
+    public static UniversalDefinition CreateMock(
+        Guid id,
+        string manufacturerName = "Acme Corp",
+        string productName = "Super Synth",
+        string patchName = "Epic Lead" )
+    {
+        return UniversalDefinition.Create(
+            id: id,
+            author: "John Doe",
+            manufacturerName: manufacturerName,
+            productName: productName,
+            patchName: patchName,
+            description: "multi-line\ndescription",
+            extra: new Dictionary<string, string>
+            {
+                { "GlobalKey1", "GlobalValue1" },
+                { "GlobalKey2", "GlobalValue2" }
+            },
+            articulations:
+            [
+                Articulation.Create(
+                    name: "Sustain",
+                    midiMessages:
+                    [
+                        // Note On
+                        MidiMessage.Create( 0x90, 40, 100 ),
+                        // Note Off
+                        MidiMessage.Create( 0x80, 40, 110 ),
+                        // Control Change
+                        MidiMessage.Create( 0xB0, 1, 127 ),
+                        // Program Change
+                        MidiMessage.Create( 0xC0, 49 ),
+                    ],
+                    extra: new Dictionary<string, string>
+                    {
+                        { "LocalKey", "LocalValue" }
+                    }
+                )
+            ]
+        );
+    }
+}
