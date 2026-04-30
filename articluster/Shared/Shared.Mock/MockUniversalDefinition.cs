d66c1ca1d69461d969c68516b3cdbc8f5fda610c
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-04-30 18:01:39 +0900

    ユニットテスト共通のユーティリティとモックの作成

---
+++ articluster/Shared/Shared.Mock/MockUniversalDefinition.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+
+using ArtiCluster.Shared.Domain.MidiMessages.Model;
+using ArtiCluster.Shared.Domain.UniversalDefinitions;
+using ArtiCluster.Shared.Domain.UniversalDefinitions.Model;
+
+namespace ArtiCluster.Shared.Mock;
+
+public static class MockUniversalDefinition
+{
+    public static UniversalDefinitionProductCollection CreateCollection()
+    {
+        return new UniversalDefinitionProductCollection(
+            [
+                Create( Guid.NewGuid(), manufacturerName: "Acme Corp", productName: "Super Synth", patchName: "Epic Lead" ),
+                Create( Guid.NewGuid(), manufacturerName: "Acme Corp X", productName: "Great Synth", patchName: "Epic Lead" ),
+                Create( Guid.NewGuid(), manufacturerName: "Acme Corp Y", productName: "Better Synth", patchName: "Epic Lead" ),
+                Create( Guid.NewGuid(), manufacturerName: "Acme Corp Y", productName: "Goog Synth", patchName: "Epic Lead" ),
+            ]
+        );
+    }
+
+    public static UniversalDefinitionManufacturerSet CreateManufacturerSet(
+        string manufacturerName = "Acme Corp" )
+    {
+        return UniversalDefinitionManufacturerSet.Create(
+            manufacturerName: manufacturerName,
+            items:
+            [
+                Create( Guid.NewGuid(), manufacturerName ),
+                Create( Guid.NewGuid(), manufacturerName, productName: "Super Guitar", patchName: "Guitar1" ),
+                Create( Guid.NewGuid(), manufacturerName, productName: "Super Bass", patchName: "E.Bass" ),
+            ]
+        );
+    }
+
+    public static UniversalDefinitionProductSet CreateProductSet(
+        string manufacturerName = "Acme Corp",
+        string productName = "Super Synth" )
+    {
+        return UniversalDefinitionProductSet.Create(
+            manufacturerName: manufacturerName,
+            productName: productName,
+            items:
+            [
+                Create( Guid.NewGuid(), manufacturerName, productName ),
+                Create( Guid.NewGuid(), manufacturerName, productName, patchName: "Warm Pad" )
+            ]
+        );
+    }
+
+    public static UniversalDefinition Create(
+        Guid id,
+        string manufacturerName = "Acme Corp",
+        string productName = "Super Synth",
+        string patchName = "Epic Lead" )
+    {
+        return UniversalDefinition.Create(
+            id: id,
+            author: "John Doe",
+            manufacturerName: manufacturerName,
+            productName: productName,
+            patchName: patchName,
+            description: "multi-line\ndescription",
+            extra: new Dictionary<string, string>
+            {
+                { "GlobalKey1", "GlobalValue1" },
+                { "GlobalKey2", "GlobalValue2" }
+            },
+            articulations:
+            [
+                Articulation.Create(
+                    name: "Sustain",
+                    midiMessages:
+                    [
+                        // Note On
+                        MidiMessage.Create( 0x90, 40, 100 ),
+                        // Note Off
+                        MidiMessage.Create( 0x80, 40, 110 ),
+                        // Control Change
+                        MidiMessage.Create( 0xB0, 1, 127 ),
+                        // Program Change
+                        MidiMessage.Create( 0xC0, 49 ),
+                    ],
+                    extra: new Dictionary<string, string>
+                    {
+                        { "LocalKey", "LocalValue" }
+                    }
+                )
+            ]
+        );
+    }
+}
