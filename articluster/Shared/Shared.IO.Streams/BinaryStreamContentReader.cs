ee365d11bd75300b26929402701e547d270de4b7
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-04-30 18:12:27 +0900

    cleanup unused import

--- articluster/Shared/Shared.IO.Streams/BinaryStreamContentReader.cs
+++ articluster/Shared/Shared.IO.Streams/BinaryStreamContentReader.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
-using ArtiCluster.Commons.Extensions;
 using ArtiCluster.Shared.IO.Abstractions;
 using ArtiCluster.Shared.IO.Abstractions.Values;
 
--- articluster/Shared/Shared.IO.Streams/TextStreamContentReader.cs
+++ articluster/Shared/Shared.IO.Streams/TextStreamContentReader.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-using ArtiCluster.Commons.Extensions;
 using ArtiCluster.Shared.IO.Abstractions;
 using ArtiCluster.Shared.IO.Abstractions.Values;
 
