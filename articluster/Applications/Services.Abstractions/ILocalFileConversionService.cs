363c740baac9bd37d850b30f6536f0f1f89a3236
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-05-16 15:58:33 +0900

    Cleanup using

--- articluster/Applications/Services.Abstractions/ILocalFileConversionService.cs
+++ articluster/Applications/Services.Abstractions/ILocalFileConversionService.cs
@@ -3,7 +3,6 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using ArtiCluster.Commons;
-using ArtiCluster.Shared.Domain.UniversalDefinitions;
 using ArtiCluster.Shared.Domain.UniversalDefinitions.Model;
 
 namespace ArtiCluster.Applications.Services.Abstractions;
