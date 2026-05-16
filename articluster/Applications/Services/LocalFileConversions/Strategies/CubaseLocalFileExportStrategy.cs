bf872478cc2c1fda27020fd0c72857c64efd262a
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-05-16 17:59:33 +0900

    出力ファイル名修正

--- articluster/Applications/Services/LocalFileConversions/Strategies/CubaseLocalFileExportStrategy.cs
+++ articluster/Applications/Services/LocalFileConversions/Strategies/CubaseLocalFileExportStrategy.cs
@@ -18,7 +18,7 @@ public sealed class CubaseLocalFileExportStrategy : ILocalFileExportStrategy<Sep
         => Path.Combine( baseDirectory, "Cubase", source.ManufacturerName.Value, source.ProductName.Value );
 
     public string GetExportFileName( SeparatedArticulationGroupSet source )
-        => $"{source.PatchName.Value} {source.ArticulationGroupName}.expressionmap";
+        => $"{source.ArticulationGroupName}.expressionmap";
 
     public async Task<Result<Unit, ExportFailureReason>> ExportAsync(
         ITextContentWriter writer,
