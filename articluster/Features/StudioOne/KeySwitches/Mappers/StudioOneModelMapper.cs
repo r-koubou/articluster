5545ca036e9dc3d9f335a6c19c4b8765ecc36a40
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-05-16 14:25:15 +0900

    ArticulationGroup追加分: StudioOne のエクスポート反映

--- articluster/Features/StudioOne/KeySwitches/Mappers/StudioOneModelMapper.cs
+++ articluster/Features/StudioOne/KeySwitches/Mappers/StudioOneModelMapper.cs
@@ -72,26 +72,29 @@ public sealed class StudioOneModelMapper
 
         foreach( var definition in source.Items )
         {
-            if( definition.Articulations.Count == 0 )
+            foreach( var group in definition.ArticulationGroups )
             {
-                continue;
+                if( group.Articulations.Count == 0 )
+                {
+                    continue;
+                }
+
+                var folder = new ElementAttribute
+                {
+                    Folder = "1",
+                    Name   = group.Name.Value
+                };
+
+                // ReSharper disable once ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator
+                foreach( var articulation in group.Articulations )
+                {
+                    var attributeElement = MapElementAttribute( articulation, assignId );
+                    folder.Children.Add( attributeElement );
+                    assignId++;
+                }
+
+                rootElement.AttributeElements.Add( folder );
             }
-
-            var folder = new ElementAttribute
-            {
-                Folder = "1",
-                Name   = definition.PatchName.Value
-            };
-
-            // ReSharper disable once ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator
-            foreach( var articulation in definition.Articulations )
-            {
-                var attributeElement = MapElementAttribute( articulation, assignId );
-                folder.Children.Add( attributeElement );
-                assignId++;
-            }
-
-            rootElement.AttributeElements.Add( folder );
         }
 
         return Result<StudioOneRootElement, ExportFailureReason>.Success( rootElement );
@@ -103,12 +106,15 @@ public sealed class StudioOneModelMapper
 
         foreach( var definition in sources )
         {
-            foreach( var articulation in definition.Articulations )
+            foreach( var group in definition.ArticulationGroups )
             {
-                var attr = MapElementAttribute( articulation, assignId );
-                assignId++;
+                foreach( var articulation in group.Articulations )
+                {
+                    var attr = MapElementAttribute( articulation, assignId );
+                    assignId++;
 
-                result.Add( attr );
+                    result.Add( attr );
+                }
             }
         }
 
