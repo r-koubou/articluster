12e0ef3d78f2b958fedcd005125f521237dabd36
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-05-16 14:19:03 +0900

    ArticulationGroup追加分: Cubase のエクスポート反映

--- articluster/Features/Cubase/ExpressionMaps/Facades/CubaseDefinitionFacade.cs
+++ articluster/Features/Cubase/ExpressionMaps/Facades/CubaseDefinitionFacade.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using ArtiCluster.Commons;
 using ArtiCluster.Features.Cubase.ExpressionMaps.Contracts;
 using ArtiCluster.Features.Cubase.ExpressionMaps.Exports;
-using ArtiCluster.Shared.Domain.UniversalDefinitions.Model;
+using ArtiCluster.Shared.Domain.UniversalDefinitions;
 using ArtiCluster.Shared.IO.Abstractions;
 
 namespace ArtiCluster.Features.Cubase.ExpressionMaps.Facades;
@@ -13,7 +13,7 @@ public sealed class CubaseDefinitionFacade : ICubaseDefinitionFacade
 {
     public async Task<Result<Unit, ExportFailureReason>> ExportAsync(
         ITextContentWriter writer,
-        UniversalDefinition source,
+        SeparatedArticulationGroupSet source,
         CancellationToken cancellationToken = default )
     {
         var exporter = new CubaseExporter();
