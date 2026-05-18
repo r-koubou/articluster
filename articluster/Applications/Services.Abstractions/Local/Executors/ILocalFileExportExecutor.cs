3c3e003e52b89fece25dffd31f026865ac5f9737
Hiroaki@R-koubou <dev.r.koubou@gmail.com>
2026-05-18 14:17:01 +0900

    Runner, Executor を抽象化し基底インターフェース作成

--- articluster/Applications/Services.Abstractions/Local/Executors/ILocalFileExportExecutor.cs
+++ articluster/Applications/Services.Abstractions/Local/Executors/ILocalFileExportExecutor.cs
@@ -1,19 +1,4 @@
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-
-using ArtiCluster.Applications.Services.Abstractions.Local.Strategies;
-using ArtiCluster.Commons;
-
 namespace ArtiCluster.Applications.Services.Abstractions.Local.Executors;
 
 public interface ILocalFileExportExecutor<TSource>
-{
-    Task<Result<Unit, ExportFailureReason>> ExecuteAsync(
-        string baseOutputDirectory,
-        IEnumerable<TSource> sources,
-        ILocalOutputNamingStrategy<TSource> outputNamingStrategy,
-        ILocalFileExportStrategy<TSource> exportStrategy,
-        CancellationToken cancellationToken = default
-    );
-}
+    : ILocalOutputExecutor<TSource, ExportFailureReason>;
---
+++ articluster/Applications/Services.Abstractions/Local/Executors/ILocalOutputExecutor.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using ArtiCluster.Applications.Services.Abstractions.Local.Strategies;
+using ArtiCluster.Commons;
+
+namespace ArtiCluster.Applications.Services.Abstractions.Local.Executors;
+
+public interface ILocalOutputExecutor<TSource, TFailureReason>
+{
+    Task<Result<Unit, TFailureReason>> ExecuteAsync(
+        string baseOutputDirectory,
+        IEnumerable<TSource> sources,
+        ILocalOutputNamingStrategy<TSource> outputNamingStrategy,
+        ILocalFileExportStrategy<TSource> exportStrategy,
+        CancellationToken cancellationToken = default
+    );
+}
